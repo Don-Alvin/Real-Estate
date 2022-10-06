e27d3f500fc3af2b6f2b60c271bb0aea11db1f15
Don-Alvin <alvindon41@gmail.com>
2022-10-06 12:03:09 +0300

    All files deleted

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import _ from "lodash";
-
-const component = () => {
-	const element = document.createElement("div");
-	element.innerHTML = _.join(["Hello", "webpack"]);
-	return element;
-};
-
-document.body.appendChild(component());
